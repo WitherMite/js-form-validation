b021960c716f217709816d731a8674a816677f9d
WitherMite <43625516+WitherMite@users.noreply.github.com>
2024-07-02 12:41:22 -0500

    Add basic validation

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
 import "./style.css";
+
+const form = document.forms[0];
+const email = document.getElementById("email");
+const country = document.getElementById("country");
+const zipCode = document.getElementById("zip");
+const password = document.getElementById("pwd");
+const confirmPass = document.getElementById("pwd-conf");
+
+[email, country, zipCode, password, confirmPass].forEach((input) =>
+  input.addEventListener("change", input.reportValidity)
+);
+
+form.addEventListener("submit", (e) => {
+  if (!form.reportValidity()) {
+    e.preventDefault();
+  } else {
+    alert("form submitted!");
+  }
+});
